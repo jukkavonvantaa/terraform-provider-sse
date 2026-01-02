fa680e602825d701071a69cd5317663633bc6ab1
jukkavonvantaa <leka@bnlab.net>
2026-01-02 18:14:45 +0200

    semantic versioning

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
-	version string = "0.13"
+	version string = "0.3.0"
 
 	// goreleaser can pass other information to the main package, such as the specific commit
 	// https://goreleaser.com/cookbooks/using-main.version/
